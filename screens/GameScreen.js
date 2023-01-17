7bd1bf3ace53a880cad2eb9fdb34e9673b545fbd
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2023-01-17 09:54:59 -0500

    Created reusable Title Component and with children and migrated styles over and replace old <Text> in GameScreen. Also Added Color Pallete for easy styles management

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,9 +1,10 @@
 import { Text, View, StyleSheet } from 'react-native';
+import Title from '../components/Title';
 
 function GameScreen() {
   return (
     <View style={styles.screen}>
-      <Text>Game Screen!</Text>
+      <Title>Opponent's Guess</Title>
       <View>
         <Text>Higher or Lower?</Text>
       </View>
@@ -19,6 +20,15 @@ export default GameScreen;
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
+    padding: 24,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#4e0329',
+    textAlign: 'center',
+    borderWidth: 2,
+    borderColor: '#4e0329',
     padding: 12,
   },
 });
